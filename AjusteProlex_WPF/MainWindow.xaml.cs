2594fe11142c2eb8af809b0deae54391570cbcc1
Bruno Menezes <brunomenezesmg@gmail.com>
2017-07-03 18:05:40 -0300

    Update

--- AjusteProlex_WPF/MainWindow.xaml.cs
+++ AjusteProlex_WPF/MainWindow.xaml.cs
@@ -240,43 +240,31 @@ namespace AjusteProlex_WPF
 
         static string GetMd5FileHash(MD5 md5Hash, string input)
         {
-
             byte[] data = md5Hash.ComputeHash(File.ReadAllBytes(input));
 
             StringBuilder sBuilder = new StringBuilder();
             for (int i = 0; i < data.Length; i++)
                 sBuilder.Append(data[i].ToString("x2"));
 
-            // Return the hexadecimal string.
             return sBuilder.ToString();
         }
 
         static string GetMd5Hash(MD5 md5Hash, string input)
         {
-
-            // Convert the input string to a byte array and compute the hash.
             byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
 
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
             StringBuilder sBuilder = new StringBuilder();
 
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
             for (int i = 0; i < data.Length; i++)
                 sBuilder.Append(data[i].ToString("x2"));
 
-            // Return the hexadecimal string.
             return sBuilder.ToString();
         }
 
-        // Verify a hash against a string.
         static bool VerifyMd5Hash(MD5 md5Hash, string input, string hash)
         {
-            // Hash the input.
             string hashOfInput = GetMd5FileHash(md5Hash, input);
 
-            // Create a StringComparer an compare the hashes.
             StringComparer comparer = StringComparer.OrdinalIgnoreCase;
 
             if (0 == comparer.Compare(hashOfInput, hash))
